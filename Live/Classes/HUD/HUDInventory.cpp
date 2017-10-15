bba1c822341f51a2ac8f52c0f9df3ae12da74037
Samuel Wu <samwwwww@hotmail.com>
2017-10-15 18:56:45 -0400

    New background assets Reduced inventory to 6 slots

--- Live/Classes/HUD/HUDInventory.cpp
+++ Live/Classes/HUD/HUDInventory.cpp
@@ -3,16 +3,21 @@
 USING_NS_CC;
 
 HUDInventory::HUDInventory(Inventory* inventory) : inventory(inventory) {
-    LayerColor::initWithColor(Color4B(255, 255, 255, 255));
-    double inv_width = BORDER_PX + (INVENTORY_SLOT_SIZE + BORDER_PX) * MAX_ITEMS;
+    double inv_width = INVENTORY_HORIZONTAL_PADDING_PX + (INVENTORY_SLOT_SIZE +
+        INVENTORY_HORIZONTAL_PADDING_PX) * MAX_ITEMS;
     setPosition(
         Vec2(Director::getInstance()->getVisibleSize().width / 2 - inv_width / 2, 0));
-    setContentSize(Size(inv_width, INVENTORY_SLOT_SIZE + BORDER_PX * 2));
+    setContentSize(Size(inv_width, INVENTORY_SLOT_SIZE + INVENTORY_VERTICAL_PADDING_PX * 2));
+
+    Sprite* inventory_background = Sprite::create("spr_inventory_background.png");
+    inventory_background->setAnchorPoint(Vec2(0.5, 0));
+    inventory_background->setPosition(Vec2(getContentSize().width/2, 0));
+    addChild(inventory_background,-1);
 
     for (int i = 0; i < MAX_ITEMS; i++) {
         hud_items[i] = new HUDInventoryItem(
-            inventory->getItemSlot(i), BORDER_PX + (BORDER_PX + INVENTORY_SLOT_SIZE) * i,
-            BORDER_PX);
+            inventory->getItemSlot(i), INVENTORY_HORIZONTAL_PADDING_PX + (INVENTORY_HORIZONTAL_PADDING_PX + INVENTORY_SLOT_SIZE) * i,
+            INVENTORY_VERTICAL_PADDING_PX);
         addChild(hud_items[i], i);
     }
 }
--- Live/Classes/HUD/HUDMessage.cpp
+++ Live/Classes/HUD/HUDMessage.cpp
@@ -7,19 +7,19 @@ HUDMessage::HUDMessage(const std::string& message) {
     m_message = message;
 
     // Set size and position of this component
-    setPosition(Vec2(Director::getInstance()->getVisibleSize().width/2 - MSG_WIDTH/2, 60));
+    setPosition(Vec2(Director::getInstance()->getVisibleSize().width/2 - MSG_WIDTH/2, 70));
     setContentSize(Size(MSG_WIDTH,100));
 
     // Create sprite as background
-    Sprite* bg = Sprite::create("messageBackground.png");
-    bg->setPosition(Vec2(MSG_WIDTH/2, 60));
-    bg->setOpacity(100);
+    Sprite* bg = Sprite::create("spr_messaging_background.png");
+    bg->setPosition(Vec2(MSG_WIDTH/2, 63));
+    bg->setOpacity(200);
     this->addChild(bg, 0);
 
     // Create, customize, and add label
-    m_label = Label::createWithTTF(message, "fonts/emulogic.ttf", 22);
+    m_label = Label::createWithTTF(message, "fonts/emulogic.ttf", 14);
     m_label->setColor(Color3B(0, 0, 0));
-    m_label->setPosition(Vec2(MSG_WIDTH/2, 30));
+    m_label->setPosition(Vec2(MSG_WIDTH/2, MSG_PADDING));
     this->addChild(m_label, 1);
 }
 
