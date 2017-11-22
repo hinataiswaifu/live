7d003f030f3e1bc9feeb0fd0173f5a710fcba4cb
Austin Du <austindu196@gmail.com>
2017-11-21 23:41:46 -0500

    footsteps

--- Live/Classes/InputManager.cpp
+++ Live/Classes/InputManager.cpp
@@ -78,7 +78,7 @@ void InputManager::enqueueFootstep(int id) {
   if (AudioManager::getInstance()->getAudioQueueSize(id) < 1) {
     AudioManager::getInstance()->enqueueIntoAudioQueue(
       id,
-      InputManager::footsteps[0]
+      InputManager::footsteps[rand()%6]
     );
   }
 }
