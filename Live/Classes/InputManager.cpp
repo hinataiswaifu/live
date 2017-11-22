eb75a1532629c04cd7c7419b91f10e4fa8558660
Austin Du <austindu196@gmail.com>
2017-11-21 22:53:45 -0500

    footsteps

--- Live/Classes/InputManager.cpp
+++ Live/Classes/InputManager.cpp
@@ -12,7 +12,7 @@ bool InputManager::m_key_c_released = true;
 // used to track gameover state, currently used to reject keyboard input
 bool InputManager::m_game_over = false;
 MainScene* InputManager::m_scene = nullptr;
-footsteps_audio_queue_id = AudioManager::getInstance()->createNewAudioQueue();
+InputManager::footsteps_audio_queue_id = AudioManager::getInstance()->createNewAudioQueue();
 
 EventListenerKeyboard* InputManager::initializeInputManager(MainScene* scene) {
     auto kb_listener = EventListenerKeyboard::create();
