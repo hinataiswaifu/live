5417e02245ac4e632772f108876b8ffb2bb6dc17
Austin Du <austindu196@gmail.com>
2017-11-21 20:54:50 -0500

    preload bg audio

--- Live/Classes/AudioManager.cpp
+++ Live/Classes/AudioManager.cpp
@@ -14,6 +14,7 @@ void AudioManager::preloadEffect(AudioComponent clip) {
 void AudioManager::changeBackgroundMusic(AudioComponent bgm) {
   m_game_bg_music = bgm;
   engine->stopBackgroundMusic();
+  engine->preloadEffect(bgm.getFilePath());
   engine->playBackgroundMusic(bgm.getFilePath(), true);
 }
 
